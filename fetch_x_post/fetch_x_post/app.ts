90cdea278bb0109bb9273092c3f709c8e83541fc
ymzksgkz <>
2024-01-08 03:47:44 +0900

    prettierフォーマット

--- fetch_x_post/fetch_x_post/app.ts
+++ fetch_x_post/fetch_x_post/app.ts
@@ -1,13 +1,3 @@
-/**
- *
- * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
- * @param {Object} event - API Gateway Lambda Proxy Input Format
- *
- * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
- * @returns {Object} object - API Gateway Lambda Proxy Output Format
- *
- */
-
 export const lambdaHandler = async () => {
   try {
     console.log('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
